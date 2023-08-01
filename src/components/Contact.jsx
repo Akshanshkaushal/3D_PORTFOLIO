fc6b1db604cf35ab774ba3c63f7b8dbaa4d388f1
Akshansh <akshanshkausha9@gmail.com>
2023-08-01 21:01:11 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "JavaScript Mastery",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "sujata@jsmastery.pro ",
           message: form.message,
         },
         VwnO5X19bQXedw_wP
